03d1ca3b1ad042e32782e5e246e5bf09218be342
vincent-mugendi <vincentmwathi5@gmail.com>
2024-05-21 01:40:59 +0300

    added user creation schema

---
+++ backend/storage/users_schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE users_db;
+
+USE users_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
