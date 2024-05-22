aef23a5e1f03fb3fd1e0ba9259672882ed9e88a4
vincent-mugendi <vincentmwathi5@gmail.com>
2024-05-22 21:41:28 +0300

    updated SelfAssessment.jsx

--- frontend/mindful-frontend/src/components/header/SelfAssessmentHeader.jsx
+++ frontend/mindful-frontend/src/components/header/SelfAssessmentHeader.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SelfAssesmentHeader = () => {
     return (
         <>
         <h4>Mindful</h4>
+        <Link to="/dashboard">Dashboard</Link>
         </>
     )
 }
