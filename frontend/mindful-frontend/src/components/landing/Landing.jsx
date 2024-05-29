d783fda0f569e79217ccd334e58a227e903fabf4
vincent-mugendi <vincentmwathi5@gmail.com>
2024-05-29 07:16:30 +0300

    updates to Landing styling

--- frontend/mindful-frontend/src/components/landing/Landing.jsx
+++ frontend/mindful-frontend/src/components/landing/Landing.jsx
@@ -4,12 +4,14 @@ import Header from "../header/Header";
 
 import './Landing.css'
 import LandingHero from "./LandingHero";
+import LandingPromise from "./LandingPromise";
 
 const LandingPage = () => {
     return (
         <>
         <Header />
         <LandingHero />
+        <LandingPromise />
         </>
     )
 }
--- frontend/mindful-frontend/src/components/landing/LandingHero.jsx
+++ frontend/mindful-frontend/src/components/landing/LandingHero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./LandingHero.css"
+import { Link } from "react-router-dom";
+import arrowIcon from "../../assets/icons/arrow-0.svg";
+
 const LandingHero = () => {
     return (
 
@@ -7,7 +10,21 @@ const LandingHero = () => {
         <section className="hero-spacer"></section>
         <h2 className="hero-title">FRIENDLY REMINDER</h2>
         <h1 className="hero-subtitle">You Matter.</h1>
-        <p className="hero-copy">Mindful is here to empower you by offering the right tools and knowledge that you need to take charge of your mental health.</p>
+        
+        <p className="hero-copy">
+            Mindful is here to empower you by offering the right tools and knowledge that you need to take charge of your mental health.
+        </p>
+
+        <div className="register-0">
+            <Link to="/signup">
+                <button className="register-btn">Begin Here...</button>
+            </Link>
+        </div>
+
+        <div className="transition">
+            <img src={arrowIcon} alt="Aroow Icon" />
+        </div>
+        
         </>
     );
 }
