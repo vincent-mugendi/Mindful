b9708034d6a9f663ab82aa155b1387a1f9c98c35
vincent-mugendi <vincentmwathi5@gmail.com>
2024-05-29 13:34:17 +0300

    Implemented SignUp Form Design

--- frontend/mindful-frontend/src/components/signup/SignupForm.jsx
+++ frontend/mindful-frontend/src/components/signup/SignupForm.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./SignupForm.css"
+
 const SignupForm = () => {
     const [formData, setFormData] = useState({
         username: "",
@@ -49,43 +53,57 @@ const SignupForm = () => {
     };
 
     return (
-        <section>
-            <form onSubmit={handleSubmit}>
-                <h2>Create Account</h2>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                    />
+        <section className="signup">
+            <div className="card">
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <h2>Welcome to Mindful</h2>
+                    <Link to="/"><img className="logo" src={logo} alt="mindful logo" /></Link>
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
+
+                <h3>Create an acccount</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit">Sign Up</button>
+                    </div>
+                </form>
+                <div className="footer">
+                    <Link to="/login">
+                    <p>
+                        Already have an account? <span style={{ fontWeight: 'bold' }}>Log In Here</span>
+                    </p>
+                    </Link>
                 </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <button type="submit">Sign Up</button>
-                </div>
-            </form>
+            </div>
         </section>
     );
 };
