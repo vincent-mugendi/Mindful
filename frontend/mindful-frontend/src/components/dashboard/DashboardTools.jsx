fa17b409dec2a0f718e18e5286fecf69c51b7540
vincent-mugendi <vincentmwathi5@gmail.com>
2024-05-30 04:07:28 +0300

    Implemented ActionCenter UI design

--- frontend/mindful-frontend/src/components/dashboard/DashboardTools.jsx
+++ frontend/mindful-frontend/src/components/dashboard/DashboardTools.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 
 import './DashboardTools.css';
+import bookIcon from "../../assets/icons/book-icon.svg";
+import meditationIcon from "../../assets/icons/meditation-icon.svg";
+import phoneIcon from "../../assets/icons/phone-icon.svg";
 
 
 const DashboardTools = () => {
     return (
         <div className="tools">
             <h3>Tools Section</h3>
+            <div className="tools-cards">
+                <div id="card1">
+                    <h4>Journal</h4>
+                    <img src={bookIcon} alt="Book Icon" />
+                </div>
+
+
+                <div id="card2">
+                    <h4>Guided Meditation</h4>
+                    <img src={meditationIcon} alt="Book Icon" />
+                </div>
+
+
+                <div id="card3">
+                    <h4>I have an emergency</h4>
+                    <img src={phoneIcon} alt="Book Icon" />
+                </div>
+
+                
+            </div>
         </div>
     );
 }
